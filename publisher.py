4e0a03a250ff9286d8484eb753a95dd72d6586ef
donia ben sedrine <bensedrinedonia@gmail.com>
2024-03-23 14:43:51 +0100

    broker metadata

--- publisher.py
+++ publisher.py
@@ -2,12 +2,38 @@ import paho.mqtt.publish as publish
 import json
 import random
 import time
+from datetime import datetime
+
+# Counter for beekeeper IDs
+beekeeper_id_counter = 1
+
+# Function to generate random data
+def generate_data():
+
+    global beekeeper_id_counter
+
+    temperature = round(random.uniform(20.0, 30.0), 2)
+    humidity = round(random.uniform(40.0, 50.0), 2)
+    co2 = round(random.uniform(60.0, 70.0), 2)
+    weight = round(random.uniform(80.0, 90.0), 2)
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    # Increment beekeeper ID counter for next beekeeper
+    beekeeper_id = beekeeper_id_counter
+    beekeeper_id_counter += 1
+    return {
+        "beekeeper_id": beekeeper_id, 
+        "temperature": temperature, 
+        "humidity": humidity,
+        "co2": co2,
+        "weight": weight,
+        "timestamp": timestamp,
+        }
 
 while True:
-    temperature = random.uniform(20.0, 30.0)
-    data = {"temperature": temperature, "timestamp": int(time.time())}
+    data = generate_data()
 
-    publish.single("temperature", payload=json.dumps(data), hostname="127.0.0.1", port=1883)
-    print(f"Published message: {json.dumps(data)} to topic temperature")
+    publish.single("metadata", payload=json.dumps(data), hostname="127.0.0.1", port=1883)
+    print(f"Published message: {json.dumps(data)} to topic metadata")
 
     time.sleep(5)
