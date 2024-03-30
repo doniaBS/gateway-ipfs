9908cb0ccac5206236db3b75e2d47e16862746fe
donia ben sedrine <bensedrinedonia@gmail.com>
2024-03-30 13:39:53 +0100

    updated publisher code:added attributes

--- publisher.py
+++ publisher.py
@@ -4,34 +4,49 @@ import random
 import time
 from datetime import datetime
 
-# Counter for beekeeper IDs
-beekeeper_id_counter = 1
-
-# Function to generate random data
-def generate_data():
-
-    global beekeeper_id_counter
-
+# Generate initial data for 4 beekeepers
+beekeepers_data = {}
+for i in range(1, 5):
+    hive_id = i
     temperature = round(random.uniform(20.0, 30.0), 2)
     humidity = round(random.uniform(40.0, 50.0), 2)
     co2 = round(random.uniform(60.0, 70.0), 2)
     weight = round(random.uniform(80.0, 90.0), 2)
+    latitude = round(random.uniform(10.0, 50.0), 6) 
+    longitude = round(random.uniform(10.0, 50.0), 6)
+    gps_location = f"{latitude},{longitude}"  # Combine latitude and longitude
+    has_pests = random.choice([True, False]) # Generate random boolean values for hasPests
+    has_diseases = random.choice([True, False]) # Generate random boolean values for hasDiseases
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-    # Increment beekeeper ID counter for next beekeeper
-    beekeeper_id = beekeeper_id_counter
-    beekeeper_id_counter += 1
-    return {
-        "beekeeper_id": beekeeper_id, 
+    beekeepers_data[i] = {
+        "hive_id": hive_id,
         "temperature": temperature, 
         "humidity": humidity,
         "co2": co2,
         "weight": weight,
-        "timestamp": timestamp,
-        }
+        "gps_location": gps_location,
+        "hasPests": has_pests,
+        "hasDiseases": has_diseases,  
+        "timestamp": timestamp
+    }
 
 while True:
-    data = generate_data()
+    # Select a beekeeper randomly
+    beekeeper_id = random.randint(1, 4)
+    data = beekeepers_data[beekeeper_id]
+
+    # Update beekeeper's values
+    data["hive_id"] = i
+    data["temperature"] = round(random.uniform(20.0, 30.0), 2)
+    data["humidity"] = round(random.uniform(40.0, 50.0), 2)
+    data["co2"] = round(random.uniform(60.0, 70.0), 2)
+    data["weight"] = round(random.uniform(80.0, 90.0), 2)
+    data["latitude"] = round(random.uniform(10.0, 50.0), 6) 
+    data["longitude"] = round(random.uniform(10.0, 50.0), 6)
+    data["gps_location"] = f"{latitude},{longitude}"
+    data["has_pests"] = random.choice([True, False])
+    data["has_diseases"] = random.choice([True, False])
+    data["timestamp"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     publish.single("metadata", payload=json.dumps(data), hostname="127.0.0.1", port=1883)
     print(f"Published message: {json.dumps(data)} to topic metadata")
